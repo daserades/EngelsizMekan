9d010ed939a6542f2119f17af5ef1160b55ac14e
alperbayram <alperbayrm@hotmail.com>
2021-06-11 00:40:52 +0300

    v0.02

---
+++ anasayfa.php
@@ -0,0 +1,52 @@
+<?php>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/uikit.min.css" />
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+</head>
+<body>  
+
+        <div class="uk-card-body"></div>
+    </div>
+</div>
+<div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+<div class="uk-position-relative uk-light" uk-slideshow>
+
+<ul class="uk-slideshow-items">
+    <li>
+        <img src="img/im2.jpeg" alt="" uk-cover>
+    </li>
+    <li>
+        <img src="img/im2.jpeg" alt="" uk-cover>
+    </li>
+    <li>
+        <img src="img/img.jpeg" alt="" uk-cover>
+    </li>
+</ul>
+<div class="uk-position-bottom-center uk-position-small">
+    <ul class="uk-dotnav">
+        <li uk-slideshow-item="0"><a href="#">Item 1</a></li>
+        <li uk-slideshow-item="1"><a href="#">Item 2</a></li>
+        <li uk-slideshow-item="2"><a href="#">Item 3</a></li>
+    </ul>
+</div>
+
+</div>
+    <div>
+        <div class="uk-card-body">
+            <h3 class="uk-card-title">Media Left</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
+<?>
